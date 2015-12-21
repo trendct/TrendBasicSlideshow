85e2f3ca91366c8a77a295e2a3f9be80939bd46a
Jake Kara <jake@jakekara.com>
2015-12-21 15:00:54 -0800

    new file:   css/trend_basic_slider.css 	new file:   img/wikimuffins.jpg 	new file:   index.html 	new file:   js/TrendSlideshow.js 	new file:   js/bootstrap-3.3.6-dist/css/bootstrap-theme.css 	new file:   js/bootstrap-3.3.6-dist/css/bootstrap-theme.css.map 	new file:   js/bootstrap-3.3.6-dist/css/bootstrap-theme.min.css 	new file:   js/bootstrap-3.3.6-dist/css/bootstrap-theme.min.css.map 	new file:   js/bootstrap-3.3.6-dist/css/bootstrap.css 	new file:   js/bootstrap-3.3.6-dist/css/bootstrap.css.map 	new file:   js/bootstrap-3.3.6-dist/css/bootstrap.min.css 	new file:   js/bootstrap-3.3.6-dist/css/bootstrap.min.css.map 	new file:   js/bootstrap-3.3.6-dist/fonts/glyphicons-halflings-regular.eot 	new file:   js/bootstrap-3.3.6-dist/fonts/glyphicons-halflings-regular.svg 	new file:   js/bootstrap-3.3.6-dist/fonts/glyphicons-halflings-regular.ttf 	new file:   js/bootstrap-3.3.6-dist/fonts/glyphicons-halflings-regular.woff 	new file:   js/bootstrap-3.3.6-dist/fonts/glyphicons-halflings-regular.woff2 	new file:   js/bootstrap-3.3.6-dist/js/bootstrap.js 	new file:   js/bootstrap-3.3.6-dist/js/bootstrap.min.js 	new file:   js/bootstrap-3.3.6-dist/js/npm.js 	new file:   js/jquery-1.11.3.min.js 	new file:   js/main.js 	new file:   js/pym.min.js 	new file:   pym.html

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(function () {
+
+	var corridor_slideshow = new TrendSlideshow ({
+		root_div: "corridor_slider_root",
+		slider_name: "rail_corridor_122015",
+		headline: "Muffin recipe",
+		// overall caption for the slideshow. At this point, we just don't use it.
+		// good thought, but "keep it simple" won out.
+		caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "	
+			+ "Aenean non nibh orci. Nulla facilisi. Phasellus dignissim est ut "
+			+ "iaculis gravida. Aenean a quam libero. Vivamus placerat nulla ut "
+			+ "metus sodales faucibus. Nam commodo tempor nunc nec viverra. In "
+			+ "volutpat augue a nibh mattis, sed ultricies nunc aliquet. Donec "
+			+ "eget ultricies.",
+		// can somedway add "button text" as an override for individual button headlines
+		button_text: "&bull;", 
+		slides: [ {
+			//url: "img/corridor_alt1_ts.jpeg",
+			headline: "About muffins",
+			caption: "This recipe only calls for a few ingrdients",
+			html: "<div><h1>Ingredients</h1>"
+			+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "	
+			+ "Aenean non nibh orci. Nulla facilisi. Phasellus dignissim est ut "
+			+ "iaculis gravida. Aenean a quam libero. Vivamus placerat nulla ut "
+			+ "metus sodales faucibus. Nam commodo tempor nunc nec viverra. In "
+			+ "volutpat augue a nibh mattis, sed ultricies nunc aliquet. Donec "
+			+ "eget ultricies."
+			+ ""
+		},
+		{
+			//url: "img/corridor_alt2_1_ts.jpeg",
+			headline: "Recipe",
+			caption: "Prep time: 10 minutes; cook time: 30 minutes",
+			html: "<div><h1>Recipe</h1>"
+			+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "	
+			+ "Aenean non nibh orci. Nulla facilisi. Phasellus dignissim est ut "
+			+ "iaculis gravida. Aenean a quam libero. Vivamus placerat nulla ut "
+			+ "metus sodales faucibus. Nam commodo tempor nunc nec viverra. In "
+			+ "volutpat augue a nibh mattis, sed ultricies nunc aliquet. Donec "
+			+ "eget ultricies."
+			+ "</div>"
+
+		},
+		{
+			//url: "img/corridor_alt2_2_ts.jpeg",
+			headline: "Muffins!",
+			caption: "Muffins!",
+			html: "<div><img src='img/wikimuffins.jpg' /> Image <a href='https://en.wikipedia.org/wiki/Muffin#/media/File:NCI_Visuals_Food_Muffins.jpg'>from Wikipedia</a></div>"
+
+		},
+		]
+	});
+
+	corridor_slideshow.pym_child.sendHeight();
+
+})
+
